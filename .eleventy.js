3860da7f42996ff87da48d610e76420d4da0107a
Jason Mevius <49728339+jasonmevius@users.noreply.github.com>
2026-01-30 10:16:49 -0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("slug", (value) => {
+    return String(value || "")
+      .toLowerCase()
+      .trim()
+      .replace(/['"]/g, "")
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+  });
+
   return {
     dir: {
       input: "src",
